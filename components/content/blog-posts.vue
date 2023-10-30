7a7f889ace67fa2ac06c26f2b8a63e23cd6a2203
themis1986 <themis_1986@hotmail.com>
2023-10-30 23:02:19 +0200

    Query content and add mdc syntax

---
+++ components/content/blog-posts.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="not-prose">
+    <ul>
+      <li v-for="post in posts" :key="post._path">
+        <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script setup>
+const { data: posts } = await useAsyncData("blog-list", () =>
+  queryContent("/blog")
+    .where({ _path: { $ne: "/blog" } })
+    .only(["_path", "title"])
+    .find()
+);
+</script>
+
+<style lang="scss" scoped></style>
---
+++ content/blog/index.md
@@ -0,0 +1,9 @@
+---
+description: "List of all my recent blog posts"
+---
+
+# Blog Posts
+
+This is thelist of all my recent blog posts
+
+::blog-posts
