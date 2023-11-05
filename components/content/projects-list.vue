62f95cacd72df3421d638ed474b88730c4b42def
themis1986 <themis_1986@hotmail.com>
2023-11-05 21:49:14 +0200

    Convert projects page in markdown

---
+++ components/content/projects-list.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="not-prose">
+    <section v-if="pending">Loading...</section>
+    <section v-else-if="error">Something went wrong</section>
+    <section v-else>
+      <ul class="grid grid-cols-1 gap-4">
+        <li
+          v-for="repository in repos"
+          :key="repository.id"
+          class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
+        >
+          <a :href="repository.html_url" target="_blank">
+            <div class="flex items-center justify-between text-sm">
+              <div class="font-semibold">{{ repository.name }}</div>
+              <div class="">{{ repository.stargazers_count }} *</div>
+            </div>
+            <p class="text-sm">{{ repository.description }}</p>
+          </a>
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script setup>
+const { error, pending, data } = await useFetch(
+  "https://api.github.com/users/themis1986/repos"
+);
+
+const repos = computed(() =>
+  data.value.filter((repo) => repo.description).sort((a, b) => a - b)
+);
+</script>
+
+<style lang="scss" scoped></style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,8 +1,7 @@
 <template>
-  <div>
-    <h2 class="text-2xl font-semibold mb-10">Projects</h2>
-    <ProjectsList />
-  </div>
+  <article class="prose dark:prose-invert">
+    <ContentDoc path="/projects" />
+  </article>
 </template>
 
 <script setup></script>
