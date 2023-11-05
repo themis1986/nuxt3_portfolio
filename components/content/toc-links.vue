6df18d22f53c79c9f1360c4299c8587a14db6cbd
themis1986 <themis_1986@hotmail.com>
2023-11-05 22:49:34 +0200

    Add table of contents

---
+++ components/content/toc-links.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <li v-for="link in links" :key="link.id">
+      <NuxtLink
+        :to="{ path: $route.path, hash: `#${link.id}` }"
+        :class="{
+          'ml-4': level,
+          'text-green-600 dark:text-green-400': activeId === link.id,
+        }"
+      >
+        {{ link.text }}
+      </NuxtLink>
+      <TocLinks
+        :links="link.children"
+        :level="level + 1"
+        :active-id="activeId"
+      />
+    </li>
+  </ul>
+</template>
+
+<script setup>
+defineProps({
+  links: Array,
+  level: {
+    type: Number,
+    default: 0,
+  },
+  activeId: {
+    tyoe: String,
+    default: null,
+  },
+});
+
+const route = useRoute();
+</script>
+
+<style lang="scss" scoped></style>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,13 +1,53 @@
 <template>
   <article
-    class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
+    class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300 max-w-noone"
   >
-    <ContentDoc />
+    <ContentDoc v-slot="{ doc }">
+      <div class="grid grid-cols-6 gap-16">
+        <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
+          <ContentRenderer :value="doc" />
+        </div>
+        <div v-if="doc.toc" class="col-span-2 not-prose">
+          <aside class="sticky top-8">
+            <div class="font-semibold mb-2">Table of contents</div>
+            <nav>
+              <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
+            </nav>
+          </aside>
+        </div>
+      </div>
+    </ContentDoc>
   </article>
 </template>
 
 <script setup>
-const route = useRoute();
+const activeId = ref(null);
+
+onMounted(() => {
+  const callback = (entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        activeId.value = entry.target.id;
+        break;
+      }
+    }
+  };
+  const observer = new IntersectionObserver(callback, {
+    root: null,
+    threshold: 0.5,
+  });
+  const elements = document.querySelectorAll("h2, h3");
+
+  for (const element of elements) {
+    observer.observe(element);
+  }
+
+  onBeforeUnmount(() => {
+    for (const element of elements) {
+      observer.unobserve(element);
+    }
+  });
+});
 </script>
 
 <style lang="scss" scoped></style>
